82f29082cf55ac1908655f8fb0c38d75e662c2c8
Emirhan <dev.emirhangundogdu@gmail.com>
2025-08-07 17:10:36 +0300

    Solution 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(isPalindrome(-79));
+
     }
+
+    public static boolean isPalindrome(int sayi){
+        String strSayi = String.valueOf(Math.abs(sayi));
+        String tersSayi = new StringBuilder(strSayi).reverse().toString();
+        if(strSayi.equals(tersSayi)){
+            return true;
+        }else{
+            return false;
+        }
+    };
 }
