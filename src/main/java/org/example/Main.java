95a3eab35e9e221993130b03b4ec8b8eeb1f0ee8
Emirhan <dev.emirhangundogdu@gmail.com>
2025-08-09 10:55:21 +0300

    Solution3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println(isPalindrome(266));
         System.out.println(isPerfectNumber(6));
         System.out.println(isPerfectNumber(5));
+        System.out.println(numberToWords(798));
+        System.out.println(numberToWords(286));
 
     }
 
@@ -18,7 +20,7 @@ public class Main {
         }else{
             return false;
         }
-    };
+    }
     public static boolean isPerfectNumber(int sayi){
         if(sayi<0){
             return false;
@@ -36,5 +38,57 @@ public class Main {
             }
         }
         return toplam == sayi;
+    }
+    public static String numberToWords(int sayi){
+        if(sayi < 0){
+            System.out.println("invalid Value");
+        }
+        if(sayi == 0){
+            System.out.println("Zero");
+        }
+
+        String sayiStr = String.valueOf(sayi);
+        StringBuilder sonuc = new StringBuilder();
+
+        for(int i = 0; i<sayiStr.length();i++){
+            char rakam = sayiStr.charAt(i);
+
+            switch (rakam) {
+                case '0':
+                    sonuc.append("Zero");
+                    break;
+                case '1':
+                    sonuc.append("One");
+                    break;
+                case '2':
+                    sonuc.append("Two");
+                    break;
+                case '3':
+                    sonuc.append("Three");
+                    break;
+                case '4':
+                    sonuc.append("Four");
+                    break;
+                case '5':
+                    sonuc.append("Five");
+                    break;
+                case '6':
+                    sonuc.append("Six");
+                    break;
+                case '7':
+                    sonuc.append("Seven");
+                    break;
+                case '8':
+                    sonuc.append("Eight");
+                    break;
+                case '9':
+                    sonuc.append("Nine");
+                    break;
+            }
+            if (i < sayiStr.length() - 1) {
+                sonuc.append(" ");
+            }
+        }
+        return sonuc.toString();
     }
 }
